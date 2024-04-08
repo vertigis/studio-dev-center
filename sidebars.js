482a6e77739a98fabd049b5252782a16d32d849c
Michael Widmer <michael.widmer@vertigis.com>
2024-04-08 16:45:04 +0200

    Add documentation for custom activities on VertiGIS Studio Desktop

--- sidebars.js
+++ sidebars.js
@@ -160,6 +160,7 @@ module.exports = {
                 "workflow/sdk-net-overview",
                 "workflow/sdk-net-register-activities",
                 "workflow/sdk-net-integrating-the-arcgis-api",
+                "workflow/sdk-net-integrating-the-arcgis-pro-api",
                 {
                     type: "category",
                     label: "VertiGIS Studio Mobile",
@@ -178,6 +179,15 @@ module.exports = {
                                 "workflow/tutorial-mobile-activity-indicator-form-element",
                             ],
                         },
+                    ],
+                },
+                {
+                    type: "category",
+                    label: "VertiGIS Studio Desktop",
+                    items: [
+                        "workflow/sdk-desktop-overview",
+                        "workflow/sdk-desktop-create-activity",
+                        "workflow/sdk-desktop-wrapping",
                     ],
                 },
                 {
